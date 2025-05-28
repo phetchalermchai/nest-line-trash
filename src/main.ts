fec277fd886e455959344762302a5894e8f22ec8
Chalermchai Wauwai <phetangelb@gmail.com>
2025-05-28 15:09:00 +0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: ['https://next-line-trash.vercel.app'], // หรือใช้ '*' ชั่วคราวเพื่อ debug
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server is running on ${process.env.PORT ?? 3000}`);
 }
