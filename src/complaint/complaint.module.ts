921a9e67b7544b89f0725feb0de442e928e8de6f
Chalermchai Wauwai <phetangelb@gmail.com>
2025-05-28 16:02:57 +0700

    commit

--- src/complaint/complaint.module.ts
+++ src/complaint/complaint.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ComplaintController } from './complaint.controller';
 import { ComplaintService } from './complaint.service';
 import { StorageModule } from '../storage/storage.module';
+import { LineModule } from '../line/line.module';
 
 @Module({
-  imports: [StorageModule],
+  imports: [StorageModule, LineModule],
   controllers: [ComplaintController],
   providers: [ComplaintService],
   exports: [ComplaintService],
