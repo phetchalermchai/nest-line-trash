3c7f33cc11ee0bf448d36361ec4a9200f26c59ce
Chalermchai Wauwai <phetangelb@gmail.com>
2025-05-29 10:06:30 +0700

    commit

--- src/complaint/complaint.controller.ts
+++ src/complaint/complaint.controller.ts
@@ -1,29 +1,47 @@
-import { Body, Controller, Param, Post, Put, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Param,
+    Post,
+    Put,
+    UploadedFiles,
+    UploadedFile,
+    UseInterceptors,
+    BadRequestException,
+} from '@nestjs/common';
 import { ComplaintService } from './complaint.service';
 import { StorageService } from '../storage/storage.service';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { randomUUID } from 'crypto';
-import { BadRequestException } from '@nestjs/common';
 
 @Controller('complaints')
 export class ComplaintController {
-    constructor(private readonly complaintService: ComplaintService, private readonly storageService: StorageService,) { }
+    constructor(
+        private readonly complaintService: ComplaintService,
+        private readonly storageService: StorageService,
+    ) { }
 
     @Post()
     @UseInterceptors(FilesInterceptor('images'))
     async create(
         @UploadedFiles() files: Express.Multer.File[],
-        @Body() body: {
+        @Body()
+        body: {
             lineUserId: string;
             description: string;
             location?: string;
         },
     ) {
+        // ✅ ตรวจว่ามีรูปไหม
+        if (!files || files.length === 0) {
+            throw new BadRequestException('ต้องแนบรูปอย่างน้อย 1 รูป');
+        }
+
         const imageUrls = await Promise.all(
             files.map(async (file) => {
                 const filename = `complaint-${randomUUID()}.jpg`;
                 return await this.storageService.uploadImage(file.buffer, filename);
-            })
+            }),
         );
 
         return this.complaintService.createComplaint({
@@ -38,8 +56,8 @@ export class ComplaintController {
     }
 
     @Post('upload')
-    @UseInterceptors(FilesInterceptor('file'))
-    async uploadImage(@UploadedFiles() file: Express.Multer.File) {
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadImage(@UploadedFile() file: Express.Multer.File) {
         if (!file) {
             throw new BadRequestException('Missing file');
         }
